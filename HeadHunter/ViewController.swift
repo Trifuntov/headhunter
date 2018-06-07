5b3c84d19a635010daf517b38afcfcc4b242e075
Igor Trifuntov <igor.trifuntov@gmail.com>
2018-06-07 14:34:55 +0700

    new

--- HeadHunter/ViewController.swift
+++ HeadHunter/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import WebKit
-var hh = "https://hh.ru"
+
+let baseUrlString = "https://hh.ru"
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: hh)
+        let url = URL(string: baseUrlString)
         let request = URLRequest(url: url!)
         webView.load(request)
     }
@@ -23,29 +24,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     
-    func loadUrl() {
-        let url = URL(string: hh)
-        let request = URLRequest(url: url!)
+    func loadUrl(_ urlString: String) {
+        guard let url = URL(string: urlString) else { // check URL
+            return
+        }
+        let request = URLRequest(url: url)
         webView.load(request)
     }
     
-    func validateUrl (urlString: String?) -> Bool { //check URL
-        let url: NSURL? = NSURL(string: urlString!)
-        if url != nil {
-            return true
-        }
-        return false
-    }
-    
     @IBAction func search(_ sender: UITextField) { // search by number
-        let searchNumber = textNumber.text!
-        hh = "https://hh.ru/vacancy/\(searchNumber)"
-        if validateUrl(urlString: hh) {
-            loadUrl()
-            textNumber.text = ""
-        } else {
-            textNumber.text = ""
+        guard let searchNumber = textNumber.text else {
+            return
         }
+        let searchString = baseUrlString + "/vacancy/\(searchNumber)"
+        loadUrl(searchString)
+        textNumber.text = ""
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { // hide the keyboard
