46b3e1e21f16830fc628bf93811c5cd8a9d9d582
Igor Trifuntov <igor.trifuntov@gmail.com>
2018-06-07 12:29:14 +0700

    new

--- HeadHunter/ViewController.swift
+++ HeadHunter/ViewController.swift
@@ -8,27 +8,33 @@
 
 import UIKit
 import WebKit
-class ViewController: UIViewController {
+var hh = "https://hh.ru"
 
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: hh)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+    
     @IBOutlet weak var textNumber: UITextField!
     
     @IBOutlet weak var webView: WKWebView!
     
-    
-    //let url = "https://hh.ru"
- 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let url = URL(string: "https://hh.ru")
-        let request = URLRequest(url: url!)
-        
-        webView.load(request)
-        
-       
+    @IBAction func search(_ sender: UITextField) { // search by number
+        let searchNumber = textNumber.text!
+        print("\(searchNumber)")
+        hh = "https://hh.ru/vacancy/\(searchNumber)"
+        viewDidLoad()
+        return textNumber.text = ""
     }
     
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { // hide the keyboard
+        view.endEditing(true)
+    }
+    
+    
 }
 
